e13225a96924075e4394e7afd0e9271b3f085a73
jac0blaw <lawsonjaco1@myvuw.ac.nz>
2023-11-26 22:54:44 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,8 +1,7 @@
 //your parameter variables go here!
-let res = 20;//resolution
-let t = 10;
-let space = 3; //3 for space and size for regular Perlin field;
-let size = 2;//size of object
+let res = 105;//resolution
+let space = 4; //Size Perlin field;
+let size = 3;//size of object
 var r = random(255)
 
 
@@ -18,14 +17,13 @@ function setup_wallpaper(pWallpaper) {
 }
   
 function wallpaper_background() {
-  background(1, 1, 1); //light honeydew green colour;
+  background(4, 100, 200); //light honeydew green colour;
   for (x = 0; x < width; x += space) {// x is equal to 0 when x is smaller then width plus or equal space in perlin field
     for (y = 0; y < height; y += space) { //y is equal to 0 when y is smaller then height plus or equal space in perlin field
-      var n = noise(x * res, y * res, t);//noise  = n = resolution times y and x and t+= #
-      stroke(n * 200, n*20, n*200, 100);//colour
+      var n = noise(x * res, y * res);//noise  = n = resolution times y and x 
+      stroke(n * 100, n * 20, n * 123, 100);//colour times noise using stroke
       strokeWeight(12); //stroke
-      //fill(n*255,n*255,n*255,255);
-      if (n<0.5){
+      if (n<10){
       line(x, y, x + size, y + size); //object
       }
     }    
@@ -38,6 +36,13 @@ function Draw(){
 
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
+  //textSize(23);
+  //textAlign(RIGHT);
+  //text('a', 50, 30);
+  //textAlign(CENTER);
+  //text('b', 50, 50);
+  //textAlign(LEFT);
+  //text('c', 50, 70);
 
-
+ 
 }
