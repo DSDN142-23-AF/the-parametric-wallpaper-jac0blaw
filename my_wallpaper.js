3d81a152b8ef91efc25efa1bc2b3b7a8e5389f44
jac0blaw <lawsonjaco1@myvuw.ac.nz>
2023-11-28 21:56:57 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,29 +1,29 @@
 //your parameter variables go here!
-let noiseScale = 20;//resolution
-let space = 4; //Size Perlin field;
-let size = 2;//size of object
-let noiseLevel = 0.2;
+let noiseScale = 100;//noise scale
+let space = 5; //Size Perlin field;
+let size = 10; //size of object
+let noiseLevel = 1;
 
 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GLIDE_WALLPAPER);
-  pWallpaper.resolution(NINE_LANDSCAPE);
+  pWallpaper.resolution(A4);
   pWallpaper.show_guide(false); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 20;
+  pWallpaper.grid_settings.row_offset  = 50;
 }
   
 function wallpaper_background() {
-  background(100, 100, 100); //bg
+  background(113, 100, 155); //bg
   for (x = 0; x < width; x += space) { //scale input cords
     for (y = 0; y < height; y += space) { //scale input cords
       var n = noiseLevel * noise (x * noiseScale, y * noiseScale);
-      stroke(n * 200, n * 200, n * 255, 100);//colour times noise using stroke
-      strokeWeight(12); //strokeweight
+      stroke(n * 105, n * 200, n * 10, 20);//colour times noise using stroke
+      strokeWeight(15); //strokeweight
       if (n<0.5){ //if noise is less than
       line(x, y, x + size, y + size); //object
       }
